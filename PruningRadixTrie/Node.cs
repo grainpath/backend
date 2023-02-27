f4710009040e55ecd5763a70c20d7115be4224c8
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2023-02-27 17:37:48 -0500

    mark trie with commit

--- PruningRadixTrie/Node.cs
+++ PruningRadixTrie/Node.cs
@@ -3,6 +3,8 @@
  * 
  * Copyright (c) 2020 Wolf Garbe at https://github.com/wolfgarbe/PruningRadixTrie
  * 
+ * Commit: 7459e3c1b2a249ec21d1048f2190181cb65ce501
+ * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
