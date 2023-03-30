d38a1c63543984aac682bac03e1b702cfbf6ed19
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2023-03-30 11:35:17 -0400

    implement route controller

--- GrainPath.Api/Controllers/RouteController.cs
+++ GrainPath.Api/Controllers/RouteController.cs
@@ -1,1 +1,45 @@
+using System.Net.Mime;
+using System.Threading.Tasks;
+using GrainPath.Api.Helpers;
+using GrainPath.Application.Entities;
+using GrainPath.Application.Helpers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using RouteResponse = GrainPath.Application.Entities.RouteObject;
+
 namespace GrainPath.Api.Controllers;
+
+[ApiController]
+[Route("api/v1/route")]
+public sealed class RouteController : ControllerBase
+{
+    private readonly IAppContext _context;
+    private readonly ILogger<RouteController> _logger;
+
+    public RouteController(IAppContext context, ILogger<RouteController> logger)
+    {
+        _context = context; _logger = logger;
+    }
+
+    [HttpPost]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [Produces(MediaTypeNames.Application.Json)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult<RouteResponse>> PostAsync(RouteRequest request)
+    {
+        if (!RequestVerifier.Verify(request)) { return BadRequest(); }
+
+        var (r, e) = await RouteFinder.Find(
+            _context.Model, _context.Engine, request.source.AsWgs(),
+            request.target.AsWgs(), request.distance.Value, request.conditions);
+
+        if (e is not null) { _logger.LogError(e.message); return StatusCode(500); }
+
+        return (r is not null) ? Ok(r) : NotFound();
+    }
+}
