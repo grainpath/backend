0b82c6e9e6d6d8b24752c58c12eb3f9339f6f6ff
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2023-06-17 15:46:44 +0200

    cap number of retrieved places

--- GrainPath.Application/Handlers/PlacesHandler.cs
+++ GrainPath.Application/Handlers/PlacesHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using GrainPath.Application.Entities;
@@ -10,11 +11,17 @@ namespace GrainPath.Application.Handlers;
 /// </summary>
 public static class PlacesHandler
 {
+    private static readonly int TOTAL_SIZE = 100;
+    private static readonly int MIN_BUCKET_SIZE = 25;
+
     /// <summary>
-    /// Get a list of matching places.
+    /// Get a list of matching places. The result is capped to avoid retrieving
+    /// the entire database.
     /// </summary>
     public static Task<List<Place>> Handle(IModel model, WgsPoint center, double radius, List<Category> categories)
     {
-        return model.GetAround(center, radius, categories);
+        var bucket = Math.Max(MIN_BUCKET_SIZE, TOTAL_SIZE / categories.Count);
+
+        return model.GetAround(center, radius, categories, bucket);
     }
 }
