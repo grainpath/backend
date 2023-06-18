3edebb6eb78b66f3fe9f10f56b33f98860acf9a6
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2023-06-18 21:15:11 +0200

    add test project

---
+++ GrainPath.Application.MSTest/Heuristics/IfHeuristicTests.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Linq;
+using GrainPath.Application.Heuristics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GrainPath.Application.MSTest;
+
+[TestClass]
+public class IfHeuristicTests
+{
+    [TestMethod]
+    public void Test()
+    {
+    }
+}
