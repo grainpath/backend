43eb90fdfbcc45b33d0f84c8983dbec7a12a2245
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2023-06-18 21:14:43 +0200

    add partial oriented greedy heuristic

---
+++ GrainPath.Application/Heuristics/OgHeuristic.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using GrainPath.Application.Entities;
+using GrainPath.Application.Interfaces;
+
+namespace GrainPath.Application.Heuristics;
+
+internal sealed class OgCategory
+{
+    public int pred = 0;
+    public readonly SortedSet<int> succ = new();
+    public readonly List<SolverPlace> places = new();
+}
+
+internal static class OgCategoryFormer
+{
+    public static SortedDictionary<int, OgCategory> Form(IReadOnlyList<SolverPlace> places, List<PrecedenceEdge> precedence)
+    {
+        var result = new SortedDictionary<int, OgCategory>();
+
+        var ensureKey = (SortedDictionary<int, OgCategory> dict, int key) =>
+        {
+            if (!dict.ContainsKey(key)) { dict.Add(key, new()); }
+        };
+
+        foreach (var place in places)
+        {
+            ensureKey(result, place.Category);
+            result[place.Category].places.Add(place);
+        }
+
+        foreach (var prec in precedence)
+        {
+            ensureKey(result, prec.fr);
+            ensureKey(result, prec.to);
+
+            ++result[prec.to].pred;
+            result[prec.fr].succ.Add(prec.to);
+        }
+
+        return result;
+    }
+}
+
+/// <summary>
+/// Oriented Greedy Heuristic from https://doi.org/10.14778/1920841.1920861.
+/// </summary>
+internal static class OgHeuristic
+{
+    public static List<int> Advise(
+        IReadOnlyList<SolverPlace> places, IDistanceMatrix matrix, List<PrecedenceEdge> precedence, double maxDistance, int placesCount)
+    {
+        var seq = new List<int>() { 0, placesCount - 1 };
+        var distance = matrix.Distance(0, placesCount - 1);
+
+        var cats = OgCategoryFormer.Form(places, precedence);
+
+        while (cats.Count > 0)
+        {
+            // TODO: (!)
+        }
+
+        return seq;
+    }
+}
---
+++ GrainPath.Application/Solvers/PrecedenceSolver.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using GrainPath.Application.Entities;
+using GrainPath.Application.Heuristics;
+using GrainPath.Application.Interfaces;
+
+namespace GrainPath.Application.Solvers;
+
+internal static class PrecedenceSolver
+{
+    public static List<List<int>> Solve(
+        IReadOnlyList<Place> places, IDistanceMatrix matrix, List<PrecedenceEdge> precedence, double maxDistance, int routesCount)
+    {
+        var routes = new List<List<int>>();
+        var solverPlaces = PlaceConverter.Convert(places);
+
+        for (int i = 0; i < routesCount; ++i)
+        {
+            var ogRoute = OgHeuristic.Advise(solverPlaces, matrix, precedence, maxDistance, places.Count);
+        }
+
+        return routes;
+    }
+}
